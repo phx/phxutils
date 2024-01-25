541e61796d1081b2181c0b0a612febbf37ee8f84
phx <oldjamey@gmail.com>
2024-01-24 21:29:56 -0600

    updated build version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ binfiles = glob.glob('bin/*')
 
 setuptools.setup(
     name='phxutils',
-    version='1.1.54',
+    version='1.1.55',
     scripts = binfiles,
     author='phx',
     author_email='phx@example.com',
